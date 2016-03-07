661c8dd0d9b558d11badb73d72cd0e6dc6b3fbc6
Gralvarek <james.asghar@gmail.com>
2016-03-07 02:48:35 -0600

    plotting 2 functions

--- CollegeBudgeting/CollegeBudgeting.py
+++ CollegeBudgeting/CollegeBudgeting.py
@@ -3,19 +3,21 @@ from Function import Function
 
 def main():
     x_ = []
-    d_ = []
+    d_1 = []
+    d_2 = []
     dt = 0.01
     
     func = Function()
     x = 0
     while x < 10:
         x_.append(x)
-        function = func.savings(x, 3)
-        d_.append(function)
+        d_1.append(func.savings(x, 30))
+        d_2.append(func.transportation(x, 5, 5, 30))
         x += dt;
 
 
-    plt.plot(x_, d_)
+    plt.plot(x_, d_1)
+    plt.plot(x_, d_2)
     plt.show()
 
 
